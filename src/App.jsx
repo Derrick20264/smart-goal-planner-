df0d041c103b1ee647133288186b4e817ef4524a
derrick <derrick.mwangi2@student.moringaschool.com>
2025-07-21 22:49:48 +0300

    Update fetch URL to / goals endpoint for backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GoalForm from "./components/GoalForm";
 import GoalList from "./components/GoalList";
 
+
 function App() {
-  
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    fetch("https://smart-goal-planner-lz9m.onrender.com/goals")
+      .then((r) => r.json())
+      .then((data) => {
+        console.log("Fetched goals:", data);
+        setGoals(data);
+      })
+      .catch((error) => console.error("Fetch error:", error));
+    }, []);
+
 
   return (
     
-      <div>
+      <div className="App">
         <h1>Smart Goal Planner</h1>
-        <Goalform />
-        <GoalList />
+        <GoalList goals={goals} />
+        <GoalForm onAddGoal={(newGoal) => setGoals([...goals, newGoal])} />
       </div>
       );
 }
